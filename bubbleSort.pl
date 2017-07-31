4f698d320e2642a3d0acae24a0c675a4134b52b6
chynna <cnssevilleno@addu.edu.ph>
2017-08-01 00:53:13 +0800

    Perl bubble sort

--- bubbleSort.pl
+++ bubbleSort.pl
@@ -5,13 +5,13 @@ use warnings;
 
 sub bubbleSort{
         print "Unsorted list:  @_ \n";
-        my $swap=1; #swap is true / sorted
+        my $swap=1; #swap is true
 
-        while($swap){
+        while($swap){ #while swap is true
                 $swap = 0; #swap is false / unsorted
                 for(my $i = 1; $i < @_; $i++){
                         if($_[$i] < $_[$i-1]){
-                                $swap=1;
+                                $swap=1; #swapping is true / sorting
                                 @_[$i,$i-1]=@_[$i-1,$i];
                         }
                 }
